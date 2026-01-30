a32016cc5db390bdfe701111165d86a13886e6fb
ayush9085 <ayush1983pandey@gmail.com>
2026-01-30 13:49:06 +0530

    add initial data types and print functionality

---
+++ EXP1/dtype.py
@@ -0,0 +1,11 @@
+name = "Ayush"          
+age = 18               
+height = 188          
+isstudent = True       
+subjects = ["Maths", "Physics", "CS"] 
+
+print(name)
+print(age)
+print(height)
+print(isstudent)
+print(subjects)
