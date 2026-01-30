851afa4436c5063ea36ef96d23ab05495195c19f
ayush9085 <ayush1983pandey@gmail.com>
2026-01-30 13:49:15 +0530

    add basic arithmetic operations and print functionality

---
+++ EXP1/operations.py
@@ -0,0 +1,15 @@
+x = 9
+y = 7
+z = 0
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+floor_division = x // y
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor Division:", floor_division)
---
+++ EXP1/pythagoras.py
@@ -0,0 +1,6 @@
+a = float(input("Enter the length of base: "))
+b = float(input("Enter the length of height: "))
+
+c = (a * a + b * b) ** 0.5
+
+print("The length of the hypotenuse is:", c)
