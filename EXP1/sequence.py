dab5d31028a77db46a6390dbd88ef1eb8b7d709f
ayush9085 <ayush1983pandey@gmail.com>
2026-02-03 09:10:43 +0530

    add functionality to check if a number is present in a predefined sequence

---
+++ EXP1/sequence.py
@@ -0,0 +1,8 @@
+sequence = (10, 20, 56, 78, 89)
+
+num = int(input("Enter a number: "))
+
+if num in sequence:
+    print("The number is present in the sequence")
+else:
+    print("The number is not present in the sequence")
