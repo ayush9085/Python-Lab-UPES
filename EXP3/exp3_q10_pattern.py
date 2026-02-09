af2b7eb3a471a12c4735397b523648bf5912c7c1
ayush9085 <ayush1983pandey@gmail.com>
2026-02-09 10:39:09 +0530

    add implementation to print a specific pattern using nested loops

---
+++ EXP3/exp3_q10_pattern.py
@@ -0,0 +1,9 @@
+n = 5
+for i in range(n, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    for k in range((n - i) * 2):
+        print("*", end="")
+    for j in range(i, 0, -1):
+        print(j, end="")
+    print()
