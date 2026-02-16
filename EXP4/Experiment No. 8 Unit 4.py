eb9d16fca7b0b08456116303f092a561b70cad8f
ayush9085 <ayush1983pandey@gmail.com>
2026-02-16 08:48:37 +0530

    add implementation to perform set operations: union, intersection, and differences

---
+++ EXP4/Experiment No. 8 Unit 4.py
@@ -0,0 +1,7 @@
+S1 = {"Red", "Yellow", "Orange", "Blue"}
+S2 = {"Violet", "Blue", "Purple"}
+
+print("Union:", S1 | S2)
+print("Intersection:", S1 & S2)
+print("Difference S1 - S2:", S1 - S2)
+print("Difference S2 - S1:", S2 - S1)
